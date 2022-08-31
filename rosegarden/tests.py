251372070ae72430b52aad671be1e9a9a5d29e29
Benjamin Roos <BenRRoos@gmail.com>
2022-08-31 10:27:03 -0500

    Book test cases

--- rosegarden/tests.py
+++ rosegarden/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Book
+
+class BookModelTests(TestCase):
+    
+    def test_title_greater_than_200_characters():
+        testbook = Book(
+            title = "A really long title that is ridiculous for a book. I mean come on, people, would you really even read this far if this were printed on the front of a book displayed front and center of a second-hand type of bookstore?",
+            author_editor = "Smith, Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )

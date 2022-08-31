6b9a97cb6a8586feb266c85952a97ad93836723b
Benjamin Roos <BenRRoos@gmail.com>
2022-08-31 13:46:59 -0500

    Book model tests

--- rosegarden/models.py
+++ rosegarden/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core import validators
 
-author_regex = validators.RegexValidator(regex='^[A-z]+-?[A-z]+,{1}([A-z]+-?[A-z]+ ?[A-z]+)+$')
+author_regex = validators.RegexValidator(regex='^[A-z]+-?[A-z]+,{1}([A-z]+(-?[A-z]+)?( ?[A-z]+)+)?$')
 # Author name should be in the format Lastname,Firstname[ ][MiddleName1][ ][MiddleName2]
 # Allow for alphabetic, commas, and hyphen characters only in the author or editor name. Only 1 comma in the middle allowed.
 # Hyphens allowed between alphabetic characters, and spaces allowed between firstname alphabetic characters.
--- rosegarden/tests.py
+++ rosegarden/tests.py
@@ -15,3 +15,111 @@ class BookModelTests(TestCase):
             )
         with self.assertRaisesMessage(ValidationError, 'Ensure this value has at most 200 characters'):
             testbook.full_clean()
+    
+    def test_validator_title_does_not_exist(self):
+        testbook = Book(
+            title = "",
+            author_editor = "Smith,Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+        with self.assertRaisesMessage(ValidationError, 'This field cannot be blank.'):
+            testbook.full_clean()
+    
+    def test_validator_title_less_than_or_equal_to_200_characters(self):
+        testbook = Book(
+            title = "A pithy title",
+            author_editor = "Smith,Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+
+    def test_validator_author_greater_than_60_characters(self):
+        testbook = Book(
+            title = "A pithy title",
+            author_editor = "Smith,Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+        with self.assertRaisesMessage(ValidationError, 'Ensure this value has at most 60 characters'):
+            testbook.full_clean()
+
+    def test_validator_author_less_than_or_equal_to_60_characters(self):
+        testbook = Book(
+            title = "A pithy title",
+            author_editor = "Smith,Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+        testbook.full_clean()
+        self.AssertTrue( len(testbook.title) <= 60)
+
+    def test_validator_author_does_not_exist(self):
+        testbook = Book(
+            title = "Cool space stories",
+            author_editor = "",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+        with self.assertRaisesMessage(ValidationError, 'This field cannot be blank.'):
+            testbook.full_clean()
+    
+    def test_validator_author_regex(self):
+        #Step 1: Valid Formats
+        testbook = Book(
+            title = "A pithy title",
+            author_editor = "Smith,Joan",
+            ddc_number = 400,
+            is_literature = False,
+            is_biography = False,
+            )
+        testbook.full_clean()
+
+        testbook.author_editor = "Smyth,J"
+        testbook.full_clean()
+
+        testbook.author_editor = "Evil Corp Publishing,"
+        testbook.full_clean()
+
+        testbook.author_editor = "Smith,Jules Verne"
+        testbook.full_clean()
+    
+        testbook.author_editor = "Smith,Jules-Verne"
+        testbook.full_clean()
+
+        testbook.author_editor = "Smith,Jules-Verne Billy-Joe"
+        testbook.full_clean()
+
+        #Step 2: Various Invalid Formats
+        testbook.author_editor = "Plato"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+        
+        testbook.author_editor = "Sm1th,Kesha"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+
+        testbook.author_editor = "Smith,Ke$ha"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+        
+        testbook.author_editor = "Smyth, Jules"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+        
+        testbook.author_editor = "Smyth,Jules-"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+        
+        testbook.author_editor = "Smyth,Jules- Verne"
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()
+        
+        testbook.author_editor = "Smyth,Jules-Verne "
+        with self.assertRaisesMessage(ValidationError, 'Enter a valid value.'):
+            testbook.full_clean()

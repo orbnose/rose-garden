225011f26fe22c432504b1a859f543ec2d6ba18f
Benjamin Roos <BenRRoos@gmail.com>
2022-08-31 14:16:42 -0500

    Book model tests

--- rosegarden/models.py
+++ rosegarden/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core import validators
 
-author_regex = validators.RegexValidator(regex='^[A-z]+(-?[A-z]+)?,({1}([A-z]+(-?[A-z]+)?)?( ?[A-z]+)?)?$')
+author_regex = validators.RegexValidator(regex='^[A-z]+(-?[A-z]+)?,{1}(([A-z]+(-?[A-z]+)?)?( ?[A-z]+)?)?$')
 # Author name should be in the format Lastname,Firstname[ ][MiddleName1][ ][MiddleName2]
 # Allow for alphabetic, commas, and hyphen characters only in the author or editor name. Only 1 comma in the middle allowed.
 # Hyphens allowed between alphabetic characters, and spaces allowed between firstname alphabetic characters.
@@ -12,3 +12,4 @@ class Book(models.Model):
     ddc_number = models.DecimalField(max_digits=12, decimal_places=9)
     is_literature = models.BooleanField()
     is_biography = models.BooleanField()
+
